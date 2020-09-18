3db19e714bcf981d785d7a409b0c804ab86d6f38
arturjzapater <zapater.artur@gmail.com>
2020-09-18 11:34:21 +0200

    added heroku config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
   },
+  purge: [
+    './js/**/*.jsx',
+    './css/**/*.css',
+  ],
   theme: {
     height: {
       'v-80': '80vh',
