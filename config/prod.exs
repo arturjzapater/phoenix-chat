e09fb679f2a540e01cdc0c2a58e115e85689678a
arturjzapater <zapater.artur@gmail.com>
2020-09-18 13:18:40 +0200

    more tests

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,9 @@ config :chat, ChatWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "firebird-chat.herokuapp.com/", port: 443],
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  check_origin: ["https://firebird-chat.herokuapp.com/"]
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -54,4 +56,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
