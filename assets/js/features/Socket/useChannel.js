23d7cf9c7dcd3b674fa904a3c96d07eea0357a95
arturjzapater <zapater.artur@gmail.com>
2020-09-21 10:50:13 +0200

    added leave event

--- assets/js/features/Socket/useChannel.js
+++ assets/js/features/Socket/useChannel.js
@@ -21,6 +21,7 @@ const useChannel = channelName => {
 
     phoenixChannel.on('new_message', ({ message, user }) => dispatch(receiveMessage(message, user)))
     phoenixChannel.on('user_joined', ({ user }) => dispatch(systemMessage(`${user} joined the conversation`)))
+    phoenixChannel.on('user_left', ({ user }) => dispatch(systemMessage(`${user} left the conversation`)))
 
     return () => phoenixChannel.leave()
   }, [])
--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -1,8 +1,7 @@
 defmodule ChatWeb.RoomChannel do
   use ChatWeb, :channel
 
-  def join("room:lobby", %{"params" => %{"user" => user}} = payload, socket) do
-    IO.puts inspect(payload)
+  def join("room:lobby", %{"params" => %{"user" => user}}, socket) do
     send(self(), :after_join)
     {:ok, assign(socket, user: user)}
   end
@@ -14,12 +13,15 @@ defmodule ChatWeb.RoomChannel do
 
   def handle_in("new_message", %{"message" => message}, socket) do
     broadcast!(socket, "new_message", %{message: message, user: socket.assigns.user})
-    IO.puts inspect(socket)
     {:noreply, socket}
   end
 
   def handle_info(:after_join, socket) do
     broadcast!(socket, "user_joined", %{message: "New user joined", user: socket.assigns.user})
     {:noreply, socket}
+  end
+
+  def terminate(_reason, socket) do
+    broadcast!(socket, "user_left", %{message: "User left", user: socket.assigns.user})
   end
 end
