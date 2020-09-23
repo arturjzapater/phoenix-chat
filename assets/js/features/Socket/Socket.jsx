787d3705615d867df7a0d8a7914e831159862bfe
arturjzapater <zapater.artur@gmail.com>
2020-09-23 17:31:20 +0200

    removed unused code

--- assets/js/features/Socket/Socket.jsx
+++ assets/js/features/Socket/Socket.jsx
@@ -1,14 +1,10 @@
-import React, { createContext, useEffect, useState } from 'react'
 import { Socket } from 'phoenix'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { receiveMessage, systemMessage } from './SocketActions'
+import React, { createContext, useEffect, useState } from 'react'
 
 const SocketContext = createContext(null)
 
 const SocketProvider = ({ children }) => {
   const [ socket, setSocket ] = useState()
-  // const user = useSelector(state => state.user)
-  // const dispatch = useDispatch()
 
   useEffect(() => {
     if (!socket) {
@@ -19,30 +15,6 @@ const SocketProvider = ({ children }) => {
   })
 
   if (!socket) return null
-
-  // let socket
-  // let channel
-  // let context
-
-  // if (user && !socket) {
-  //   socket = new Socket('/socket', { params: { user } })
-  //   socket.connect()
-
-  //   channel = socket.channel('room:lobby')
-  //   channel.join()
-  //     .receive('ok', res => console.log('Joined successfully!', res))
-  //     .receive('error', res => console.log('Oopsies!', res))
-
-  //   channel.on('new_message', ({ message, user }) => dispatch(receiveMessage(message, user)))
-  //   channel.on('user_joined', ({ user }) => dispatch(systemMessage(`${user} joined the conversation`)))
-
-  //   const sendMessage = message => channel.push('new_message', { message })
-
-  //   context = {
-  //     socket,
-  //     sendMessage,
-  //   }
-  // }
 
   return (
     <SocketContext.Provider value={{ socket }}>
